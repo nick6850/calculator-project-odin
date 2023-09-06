ae52860e007142beb33a63ad3cdd89e4d28886ae
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-09-06 16:40:51 +0300

    Added basic functionality for calculator
    This commit adds the basic functionality to the calculator, enabling users to perform basic math operations and view the results in the user interface (UI). Additionally, it ensures that the calculator continues to calculate subsequent operations by treating the previous result as a new number.

--- script.js
+++ script.js
@@ -37,14 +37,54 @@ function operate(num1, operator, num2) {
 let digits = document.querySelectorAll('.digits > div')
 let num1 = document.querySelector('#num1')
 let num2 = document.querySelector('#num2')
+
 let operator = document.querySelector('#operator')
+let operators = document.querySelectorAll('.operators')
+
+let del = document.querySelector('#del')
+let eq = document.querySelector('#eq')
+let res = document.querySelector('#res')
+let eqSign = document.querySelector('#eqSign')
+
+let screenItems = document.querySelectorAll('#screen > div')
+
 
 digits.forEach(digit => {
     digit.addEventListener('click', () => {
-        if (operator.textContent == '') {
-            num1.textContent = digit.textContent
-        } else {
-            num2.textContent = digit.textContent
+        if (eq.textContent === '') {
+            if (operator.textContent == '') {
+                num1.textContent += digit.textContent
+            } else {
+                num2.textContent += digit.textContent
+            }
+        }
+})
+})
+
+operators.forEach( (sign) => {
+    sign.addEventListener('click', () => {
+        if (num2.textContent === '') {
+            operator.textContent = sign.textContent
+        }
+        if (res.textContent != '') {
+            num1.textContent = res.textContent 
+            num2.textContent = ''
+            eq.textContent = ''
+            res.textContent = ''
+            operator.textContent = sign.textContent
         }
     })
 })
+
+del.addEventListener('click', () => {
+    screenItems.forEach(item => {
+        item.textContent = ''
+    })
+})
+
+eqSign.addEventListener('click', () => {
+    if (res.textContent === '') {
+        eq.textContent = '='
+        res.textContent = operate(Number(num1.textContent), operator.textContent, Number(num2.textContent))
+    }
+}); 
